46d7367e619f88e728fd44face765cc4291148b1
Thiago <thiagolinog8@gmail.com>
2025-04-23 12:44:15 -0300

    Fix font color

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -28,39 +28,45 @@
 
 {#if game.status !== GameStatus.playing}
 	<dialog use:show transition:fade class="overlay">
-			<section>
-				<h2 class="primary">{message}</h2>
-				{#if game.status === GameStatus.choosing}
-					<div class="selector">
-						<button
-							class:secondary={game.dificulty !== GameDificulty.easy}
-							onclick={() => (game.dificulty = GameDificulty.easy)}
-						>
-							Easy
-						</button>
-						<button
-							class:secondary={game.dificulty !== GameDificulty.medium}
-							onclick={() => (game.dificulty = GameDificulty.medium)}
-						>
-							Medium
-						</button>
-						<button
-							class:secondary={game.dificulty !== GameDificulty.hard}
-							onclick={() => (game.dificulty = GameDificulty.hard)}
-						>
-							Hard
-						</button>
-					</div>
-					<button onclick={() => (game.status = GameStatus.playing)}>Play</button>
-				{:else}
-					<button onclick={() => (game.status = GameStatus.choosing)}>Change Dificulty</button>
-					<button onclick={() => (game.status = GameStatus.playing)}>Play again</button>
-				{/if}
-			</section>
+		<section>
+			<h2 class="primary">{message}</h2>
+			<h4>Use the arrow keys on desktop or touch on the four quadrants of the screen on mobile</h4>
+			{#if game.status === GameStatus.choosing}
+				<div class="selector">
+					<button
+						class:secondary={game.dificulty !== GameDificulty.easy}
+						onclick={() => (game.dificulty = GameDificulty.easy)}
+					>
+						Easy
+					</button>
+					<button
+						class:secondary={game.dificulty !== GameDificulty.medium}
+						onclick={() => (game.dificulty = GameDificulty.medium)}
+					>
+						Medium
+					</button>
+					<button
+						class:secondary={game.dificulty !== GameDificulty.hard}
+						onclick={() => (game.dificulty = GameDificulty.hard)}
+					>
+						Hard
+					</button>
+				</div>
+				<button onclick={() => (game.status = GameStatus.playing)}>Play</button>
+			{:else}
+				<button onclick={() => (game.status = GameStatus.choosing)}>Change Dificulty</button>
+				<button onclick={() => (game.status = GameStatus.playing)}>Play again</button>
+			{/if}
+		</section>
 	</dialog>
 {/if}
 
 <style>
+	h4,
+	h2 {
+		color: white;
+		text-align: center;
+	}
 	.selector {
 		display: flex;
 		gap: 1rem;
