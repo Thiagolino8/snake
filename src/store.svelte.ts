cec1ca7d4fa4bc247ee7c8a91eb7a9958a693e57
Thiago <thiagolinog8@gmail.com>
2025-04-23 11:38:29 -0300

    remove enums

--- src/store.svelte.ts
+++ src/store.svelte.ts
@@ -1,14 +1,19 @@
-export const enum GameStatus {
-	playing,
-	lost,
-	won,
-	choosing,
-}
+export const GameStatus = {
+	playing: 'playing',
+	lost: 'lost',
+	won: 'won',
+	choosing: 'choosing',
+} as const
+export type GameStatus = (typeof GameStatus)[keyof typeof GameStatus]
 
-export const enum GameDificulty {
-	easy = 10,
-	medium = 15,
-	hard = 20,
-}
+export const GameDificulty = {
+	easy: 10,
+	medium: 15,
+	hard: 20,
+} as const
+export type GameDificulty = (typeof GameDificulty)[keyof typeof GameDificulty]
 
-export const game = $state({ dificulty: GameDificulty.medium, status: GameStatus.choosing });
+export const game = $state<{ dificulty: GameDificulty; status: GameStatus }>({
+	dificulty: GameDificulty.medium,
+	status: GameStatus.choosing,
+})
